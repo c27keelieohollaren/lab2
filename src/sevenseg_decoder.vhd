4bbefb30a6a122d21b49f3a51cd46d683d744e4e
c27keelieohollaren <c27keelie.o'hollaren@afacademy.af.edu>
2025-03-02 16:27:53 -0700

    Update sevenseg_decoder.vhd

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -37,8 +37,25 @@ entity sevenseg_decoder is
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
 begin
+    with i_Hex select
+    o_seg_n <=  "1111110" when "0000",
+            "0110000" when "0001",
+            "1101101" when "0010",
+            "1111001" when "0011",
+            "0110011" when "0100",
+            "1011011" when "0101",
+            "1011111" when "0110",
+            "1110000" when "0111",
+            "1111111" when "1000",
+            "1110011" when "1001",
+            "0001000" when "1010",
+            "1100000" when "1011",
+            "0110001" when "1100",
+            "1000010" when "1101",
+            "0110000" when "1110",
+            "0111000" when "1111";
+
 
 
 end Behavioral;
