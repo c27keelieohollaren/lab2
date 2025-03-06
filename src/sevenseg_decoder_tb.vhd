ed8d432aeeecd75f0825098f297eab87dbc35bc0
c27keelieohollaren <c27keelie.ohollaren@afacademy.af.edu>
2025-03-05 17:12:55 -0700

    Update sevenseg_decoder_tb.vhd

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -34,7 +34,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity sevenseg_decoder_tb is
 end sevenseg_decoder_tb;
 
-architecture test_bench of sevenseg_decoder_tb is
+architecture Behavioral of sevenseg_decoder_tb is
     signal i_Hex: STD_LOGIC_VECTOR (3 downto 0) := "0000";
     signal o_seg_n : STD_LOGIC_VECTOR (6 downto 0);
     
@@ -47,43 +47,17 @@ begin
     uut: sevenseg_decoder port map (
         i_Hex => i_Hex,
         o_seg_n => o_seg_n);
-    test_process: process
+        
+    process
     begin
-        w_hex <= x"0"; wait for 10 ns;
-            assert w_seg_n = "0000001" report "Error: 0" severity failure;
-        w_hex <= x"1"; wait for 10 ns;
-            assert w_seg_n = "1001111" report "Error: 1" severity failure;
-        w_hex <= x"2"; wait for 10 ns;
-            assert w_seg_n = "0010010" report "Error: 2" severity failure;
-        w_hex <= x"3"; wait for 10 ns;
-            assert w_seg_n = "0000110" report "Error: 3" severity failure;
-        w_hex <= x"4"; wait for 10 ns;
-            assert w_seg_n = "1001100" report "Error: 4" severity failure;
-        w_hex <= x"5"; wait for 10 ns;
-            assert w_seg_n = "0100100" report "Error: 5" severity failure;
-        w_hex <= x"6"; wait for 10 ns;
-            assert w_seg_n = "0100000" report "Error: 6" severity failure;
-        w_hex <= x"7"; wait for 10 ns;
-            assert w_seg_n = "0001111" report "Error: 7" severity failure;
-        w_hex <= x"8"; wait for 10 ns;
-            assert w_seg_n = "0000000" report "Error: 8" severity failure;
-        w_hex <= x"9"; wait for 10 ns;
-            assert w_seg_n = "0001100" report "Error: 9" severity failure;
-        w_hex <= x"A"; wait for 10 ns;
-            assert w_seg_n = "0001000" report "Error: A" severity failure;
-        w_hex <= x"B"; wait for 10 ns;
-            assert w_seg_n = "1100000" report "Error: B" severity failure;
-        w_hex <= x"C"; wait for 10 ns;
-            assert w_seg_n = "1110010" report "Error: C" severity failure;
-        w_hex <= x"D"; wait for 10 ns;
-            assert w_seg_n = "1000010" report "Error: D" severity failure;
-        w_hex <= x"E"; wait for 10 ns;
-            assert w_seg_n = "0110000" report "Error: E" severity failure;
-        w_hex <= x"F"; wait for 10 ns;
-            assert w_seg_n = "0111000" report "Error: F" severity failure;    
+        for i in 0 to 15 loop
+            i_Hex <= std_logic_vector(to_unsigned(i,4));
+            wait for 10 ns;
+        end loop;
+        
         wait;
     end process;
 
 
 
-end test_bench;
+end Behavioral;
